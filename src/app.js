5646980814327df55d0596862f02533158b33394
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-14 20:51:04 +0900

    Get element by class properly
    Forgot the dot to mark container as a class in the jquery selection

--- src/app.js
+++ src/app.js
@@ -3,16 +3,13 @@ for(let i = 0; i < 4; i++) {
   list.push(Math.floor(Math.random() * 1000));
 }
 
-var $container = $('container');
+var $container = $('.container');
 
-_.each(list, (id) => {
+_.each(list, function(id) {
   var $div = $('<div></div>');
   $div.attr('id', id).addClass('number');
   $container.append($div);
 });
-  var $div = $('<div></div>');
-  $div.attr('id', id).addClass('number');
-  $container.append($div);
 
 console.log(list);
 console.log($container);
