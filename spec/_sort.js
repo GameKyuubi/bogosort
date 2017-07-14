34e1cf3a5788d0718a7345fc9891161802493b79
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-14 14:56:44 +0900

    Add sort number array test

--- spec/_sort.js
+++ spec/_sort.js
@@ -2,4 +2,11 @@ describe('BogoSort', () => {
   it('Should accept an array', () => {
     expect(BogoSortR, []).not.toThrow();
   });
+
+  it('should sort an array of numbers'), () => {
+    const unsortedArray = [9, 5, 2, 8, 30];
+    const sortedArray = [2, 5, 8, 9, 30];
+    const result = BogoSortR(unsortedArray);
+    expect(result).toEqual(sortedArray);
+  }
 });
