c5c6c026d2824780d56fae8cedd5e1cc0393b765
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-14 15:08:59 +0900

    Fix test bugs

--- spec/_sort.js
+++ spec/_sort.js
@@ -3,10 +3,10 @@ describe('BogoSort', () => {
     expect(BogoSortR, []).not.toThrow();
   });
 
-  it('should sort an array of numbers'), () => {
+  it('should sort an array of numbers', () => {
     const unsortedArray = [9, 5, 2, 8, 30];
     const sortedArray = [2, 5, 8, 9, 30];
     const result = BogoSortR(unsortedArray);
     expect(result).toEqual(sortedArray);
-  }
+  });
 });
