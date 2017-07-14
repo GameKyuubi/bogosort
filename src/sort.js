8d80924e673d733b85797bd89cb20a9c269d1991
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-14 15:30:03 +0900

    Add randomize function
    This function will be used to randomize the array if it is not
    sorted yet

--- src/sort.js
+++ src/sort.js
@@ -1,5 +1,15 @@
 function BogoSortR(array) {
   let sortedArray = [];
 
-  return sortedArray;
+  function randomize(array) {
+    let oldArray = array.slice();
+    let newArray = [];
+    while(oldArray.length > 0) {
+      let index = Math.floor(Math.random() * oldArray.length);
+      newArray.push(oldArray.splice(index, 1)[0]);
+    }
+    return newArray;
+  }
+
+  return tempArray;
 }
