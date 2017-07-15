b4b5a46dc9c0d137827bed360cb4fde6c212edc2
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-15 15:51:57 +0900

    Add trails to visuals, rotate to vertical

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var unsortedList = [];
 for(let i = 0; i < 6; i++) {
-  unsortedList.push(Math.floor(Math.random() * 1000));
+  unsortedList.push(Math.floor(Math.random() * 255));
 }
 
 var sortedList = _.sortBy(unsortedList);
@@ -11,25 +11,28 @@ var $sortedContainer = $('.sorted-container');
 _.each(unsortedList, function(value) {
   var $div = $('<div>' + value + '</div>');
   $div.attr('id', 'unsorted-' + value).addClass('unsorted-number number');
+  $div.css('background-color', 'rgba(0, 0, ' + value + ')');
   $unsortedContainer.append($div);
 });
 
 _.each(sortedList, function(value) {
   var $div = $('<div>' + value + '</div>');
   $div.attr('id', 'sorted-' + value).addClass('sorted-number number');
+  $div.css('background-color', 'rgba(0, 0, ' + value + ')');
   $sortedContainer.append($div);
 });
 
 var worker = new Worker('src/sort.js');
 worker.onmessage = function(list) {
-  console.log('Recieved message from worker: ' + list.data);
-  $('.unsorted-container').empty();
+  var $container = $('<div class=\'unsorted-container container\'><div>');
   _.each(list.data, function(value) {
     var $div = $('<div>' + value + '</div>');
     $div.attr('id', 'unsorted-' + value).addClass('unsorted-number number');
-    $unsortedContainer.append($div);
+    $div.css('background-color', 'rgba(0, 0, ' + value + ')');
+    $container.append($div);
   });
+  var $trailContainer = $('.trail-container');
+  $trailContainer.prepend($container);
 };
 
-console.log(worker);
 worker.postMessage(unsortedList);
