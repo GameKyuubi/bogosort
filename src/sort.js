f44c7bbde525eb6aaf6eb27bf4187377fbdf72e4
GameKyuubi <GameKyuubi@users.noreply.github.com>
2017-07-15 16:00:15 +0900

    Temp sound commit

--- src/sort.js
+++ src/sort.js
@@ -1,33 +1,4 @@
-function BogoSortR(array) {
-  let sortedArray = [];
-  let tempArray = array.slice();
-
-  while(!checkSort(tempArray)) {
-    tempArray = array.slice();
-    tempArray = randomize(tempArray);
-  }
-
-  function randomize(array) {
-    let oldArray = array.slice();
-    let newArray = [];
-    while(oldArray.length > 0) {
-      let index = Math.floor(Math.random() * oldArray.length);
-      newArray.push(oldArray.splice(index, 1)[0]);
-    }
-    return newArray;
-  }
-
-  function checkSort(array) {
-    for(let i = 0; i < array.length - 1; i++) { // if this succeeds to the end - 1 of the array, the last element has already been checked
-      if(!(array[i] <= array[i + 1])) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  return tempArray;
-}
+importScripts('../bower_components/jquery/dist/jquery.js');
 
 function BogoSortR2(array) {
   let sortedArray = [];
@@ -62,6 +33,7 @@ function BogoSortR2(array) {
     return true;
   }
 
+  $.playSound('../mail.mp3');
   return tempArray;
 }
 
